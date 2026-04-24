7ae48c5d95d59a574e94c3e47a5bd3d9a4b796f6
Eren <erendev06@gmail.com>
2026-04-24 17:30:01 +0300

    fix database url problem

--- packages/app-db/src/index.ts
+++ packages/app-db/src/index.ts
@@ -2,7 +2,7 @@ import { PrismaClient } from './generated/prisma/client';
 import { PrismaPg } from '@prisma/adapter-pg';
 
 const adapter = new PrismaPg({
-  connectionString: process.env.DATABASE_URL,
+  connectionString: process.env.APP_DATABASE_URL,
 });
 
 export default new PrismaClient({ adapter });
