e92e6be0b8f7bd2ae8e4da933852cb56d1ab5356
Eren <erendev06@gmail.com>
2026-06-22 22:53:11 +0300

    fuck tpm

--- apps/ims/src/index.ts
+++ apps/ims/src/index.ts
@@ -3,32 +3,14 @@ import logger from '@sdp/config/logger';
 import cluster from './func/cluster';
 import credentials from './func/credentials';
 
-import { join } from 'path';
-import TPM2_TSS_TCTILDR from '../lib/ffi/tpm2-tss/tctildr';
-import TPM2_TSS_ESYS from '../lib/ffi/tpm2-tss/esys';
-
 const server = new Server({});
 const IMS_HOST = `${import.meta.env.IMS_HOST}:${import.meta.env.IMS_PORT}`;
-
-const tcti = new TPM2_TSS_TCTILDR(
-  import.meta.env.IMS_TSS2_TCTI || 'device:/tpm/tpm0'
-);
-
-const tpm = new TPM2_TSS_ESYS(tcti);
-const random = tpm.Esys_GetRandom(32);
-
-console.log(random.buffer);
 
 async function init() {
   await cluster(server);
   await credentials(server);
 
   logger.info('IMS Initialized');
-
-  // const tss2 = new TSS2();
-  // const bytes = tss2.Fapi_GetRandom(32);
-  // logger.info('Random bytes:');
-  // console.log(bytes);
 
   server.bindAsync(IMS_HOST, ServerCredentials.createInsecure(), (err) => {
     if (err) {
